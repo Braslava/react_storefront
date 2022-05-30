ccc27bda82b116659bcb38485ec3397432acb260
Mara Braslava <m.braslava@gmail.com>
2022-05-30 22:01:49 +0300

    add main carousel functionality and thumbnails

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -11,26 +11,46 @@ class Carousel extends Component {
         this.state = { currentImg: 0, showButtons: false };
     }
 
-    showButtons = () => {
+    showButtons = () => {};
 
-    }
     render() {
         console.log(this.props.images[this.state.currentImg]);
+        const { images, name } = this.props;
         return (
             <div>
                 <div className="carousel">
-                    <div className="carousel__thumbnails"></div>
+                    <div className="carousel__thumbnails-container">
+                        {images.map(image => {
+                            return (<img className="carousel__thumbnail" src={image} alt={name} key={image}/>)
+                        })}
+                    </div>
                     <div
                         className="carousel__inner"
                         onMouseOver={this.showButtons()}
                     >
                         <img
                             className="carousel__img--main"
-                            src={this.props.images[this.state.currentImg]}
-                            alt={this.props.name}
+                            src={images[this.state.currentImg]}
+                            alt={name}
                         />
-                        <button className="carousel__button carousel__button--left"></button>
-                        <button className="carousel__button carousel__button--right"></button>
+                        <button
+                            className="carousel__button carousel__button--left"
+                            onClick={() => {
+                                this.state.currentImg > 0 &&
+                                    this.setState({
+                                        currentImg: this.state.currentImg - 1,
+                                    });
+                            }}
+                        ></button>
+                        <button
+                            className="carousel__button carousel__button--right"
+                            onClick={() => {
+                                this.state.currentImg < images.length - 1 &&
+                                    this.setState({
+                                        currentImg: this.state.currentImg + 1,
+                                    });
+                            }}
+                        ></button>
                     </div>
                 </div>
             </div>
